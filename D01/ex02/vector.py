caafd729918d1c6b1ebf8c3cee2ca326d005c92c
Kerbault <erbault.kevin@gmail.com>
2020-05-27 22:34:07 +0200

    Quick update

--- D01/ex02/vector.py
+++ D01/ex02/vector.py
@@ -1,20 +1,39 @@
 class Vector:
-    def __init__(self):
-        self.values
-        self.size
+    def __init__(self, values):
+        self.values = values
+        self.size = values.len
 
-    def __add__:
-    def __radd__:
+    def __add__(self):
+        print()
+
+    def __radd__(self):
+        print()
         # add : scalars and vectors, can have errors with vectors.
-    def __sub__:
-    def __rsub__:
+
+    def __sub__(self):
+        print()
+
+    def __rsub__(self):
+        print()
         # sub : scalars and vectors, can have errors with vectors.
-    def __truediv__:
-    def __rtruediv__:
+
+    def __truediv__(self):
+        print()
+
+    def __rtruediv__(self):
+        print()
         # div : only scalars.
-    def __mul__:
-    def __rmul__:
+
+    def __mul__(self):
+        print()
+
+    def __rmul__(self):
+        print()
         # mul : scalars and vectors, can have errors with vectors,
         # return a scalar if we perform Vector * Vector (dot product)
-    def __str__:
-    def __repr__:
+
+    def __str__(self):
+        print()
+
+    def __repr__(self):
+        print()
